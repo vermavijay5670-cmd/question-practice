7512930be02ad4db905e97fc37de197eba176908
Vijay Verma <vermavijay5670@gmail.com>
2026-08-13 06:33:37 +0530

    bio pdf links added

--- src/data/resources.js
+++ src/data/resources.js
@@ -9,6 +9,51 @@ export const RESOURCES = {
       flashcards:
         'https://notebook.google.com/notebook/7d11822a-2997-4e81-b094-6de042470478/artifact/edeba718-2188-49f9-869e-24a14161720b?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
     },
+    'living-world': {
+      shortnotes: 'https://notebook.google.com/notebook/136e5e39-4066-42e0-97d2-b5f586013fc6/artifact/c84c9f7e-9048-492d-accb-a74924ca8940?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/136e5e39-4066-42e0-97d2-b5f586013fc6/artifact/979e38db-b83e-43eb-bd0f-abf6ef56219b?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'biological-classification': {
+      shortnotes: 'https://notebook.google.com/notebook/c8146e3b-62a2-43ce-a9fc-8715e483f896/artifact/3e91addb-4db2-4ff5-848e-52283dfd15ce?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/c8146e3b-62a2-43ce-a9fc-8715e483f896/artifact/3c466b49-6123-46e2-b6f1-129b7f62afbe?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'plant-kingdom': {
+      shortnotes: 'https://notebook.google.com/notebook/4dfdb964-1762-4b25-865f-fed3d1049b87/artifact/798a5c49-92ca-403f-95dc-f8711232ff81?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/4dfdb964-1762-4b25-865f-fed3d1049b87/artifact/d73d5e8b-22cd-4a61-9a75-8a32dc10027d?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'animal-kingdom': {
+      shortnotes: 'https://notebook.google.com/notebook/5deadb9f-4661-490c-8b39-7c07c82b681e/artifact/bc8f53fe-6bd9-4822-bfc7-659c4560ad7f?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/5deadb9f-4661-490c-8b39-7c07c82b681e/artifact/a711701a-3837-4bff-9135-42efdabd2f5f?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'cell-unit-of-life': {
+      shortnotes: 'https://notebook.google.com/notebook/8552e3a4-be60-462b-83dc-2eeb6d3b9044/artifact/6b42bc0b-9251-41a7-8561-1378774e6517?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/8552e3a4-be60-462b-83dc-2eeb6d3b9044/artifact/6a9a882e-0921-4552-80ec-5d7b00dcf6cc?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'biomolecules': {
+      shortnotes: 'https://notebook.google.com/notebook/a4126d1f-ca85-401d-8b13-684527a8f757/artifact/dc6b9f6e-eadf-4de8-a20e-fb0011224801?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/a4126d1f-ca85-401d-8b13-684527a8f757/artifact/dc6b9f6e-eadf-4de8-a20e-fb0011224801?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'cell-cycle-division': {
+      shortnotes: 'https://notebook.google.com/notebook/f5d3c0c2-5bbb-47cc-a9e3-83fd71ad2375/artifact/039ee2f7-992c-446d-acf2-20888eff1539?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/f5d3c0c2-5bbb-47cc-a9e3-83fd71ad2375/artifact/4accee3d-7cd0-4874-86ba-65047fb6c6b1?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'photosynthesis': {
+      shortnotes: 'https://notebook.google.com/notebook/d9fccc45-4c43-40db-9096-bec6ab7c9ef0/artifact/0e9df05d-06c9-420c-89c7-f34066513c6a?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/d9fccc45-4c43-40db-9096-bec6ab7c9ef0/artifact/9b50a2c0-e38b-43b3-8ed1-f0eb816404cb?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'respiration-plants': {
+      shortnotes: 'https://notebook.google.com/notebook/69f7cf0f-adce-467c-a3c8-08fc95e88156/artifact/a2fb2748-692b-4c55-8d5e-c7706cdf3b0f?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/69f7cf0f-adce-467c-a3c8-08fc95e88156/artifact/9b59fbdd-cb57-477c-b51f-abc08e571b8b?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'plant-growth-development': {
+      shortnotes: 'https://notebook.google.com/notebook/90d1d789-4bca-4bd5-88c6-01bf4ddde39e/artifact/75dde09a-b252-40a5-b281-56742050d110?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: 'https://notebook.google.com/notebook/90d1d789-4bca-4bd5-88c6-01bf4ddde39e/artifact/620d5dcf-ba6a-4091-8bf8-3acd08a1fa1c?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+     },
+     'body-fluids-circulation': {
+      shortnotes: 'https://notebook.google.com/notebook/494b4eda-def6-4127-92b8-ebf8e5faf1aa/artifact/c4684bdb-fc94-40d7-b9db-6a2e009d902e?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
+      flashcards: '',
+     },
+    
 
   },
 
@@ -29,6 +74,8 @@ export const RESOURCES = {
        shortnotes: 'YOUR LINK',
        flashcards: 'https://notebook.google.com/notebook/0d7525ff-0861-4735-ae5b-87cb24208a32/artifact/dae10c04-56b7-4310-bcd2-303aaf1ced3d?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
+     
+
 
   },
 
