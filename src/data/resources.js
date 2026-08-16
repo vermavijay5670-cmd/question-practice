0cc8ae5d57286a7186af99cf52cf42b165b1609e
Vijay Verma <vermavijay5670@gmail.com>
2026-08-16 14:19:57 +0530

    chemistry notes updated

--- src/data/resources.js
+++ src/data/resources.js
@@ -155,32 +155,29 @@ export const RESOURCES = {
       flashcards: 'https://notebook.google.com/notebook/ff533c05-5331-4a19-af8d-796b4a1e3b1e/artifact/153b1a07-9b48-4625-8e18-641ef12a405d?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
      'chemical-bonding-molecular-structure': {
-      shortnotes: '',
+      shortnotes: 'file:///E:/download2.O/coming_soon.html',
       flashcards: 'https://notebook.google.com/notebook/f50611ef-149a-4ff1-8d04-a64ca2b662f3/artifact/0b3e6600-54f3-416c-9cea-871effcca70d?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
      'thermodynamics': {
-      shortnotes: '',
+      shortnotes: 'file:///E:/download2.O/coming_soon.html',
       flashcards: 'https://notebook.google.com/notebook/3fa60767-013b-4a85-95c8-97457bc2d216/artifact/b0bf37a6-b800-482f-869b-9aaa93b52d47?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
      'equilibrium': {
-      shortnotes: '',
+      shortnotes: 'file:///E:/download2.O/coming_soon.html',
       flashcards: 'https://notebook.google.com/notebook/8ad1d1f0-0b2c-41d8-9896-fafb7a135bfe/artifact/476cafa3-92d6-48a8-8dfc-a011f9e18005?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
      'redox-reactions': {
-      shortnotes: '',
+      shortnotes: 'file:///E:/download2.O/coming_soon.html',
       flashcards: 'https://notebook.google.com/notebook/8e677bc2-dc03-42c7-a0a0-1ac1dab54ad1/artifact/0f800adf-f4d0-4ce5-8181-7345fed5c5b6?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
      'organic-chem-basic-principles-techniques': {
-      shortnotes: '',
+      shortnotes: 'file:///E:/download2.O/coming_soon.html',
       flashcards: 'https://notebook.google.com/notebook/4b9bb7d3-33e4-40db-b18a-c524ac6e4114/artifact/4a58c001-6d1d-4a0f-b13f-071d185fbb9e?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
      'hydrocarbons': {
-      shortnotes: '',
+      shortnotes: 'file:///E:/download2.O/coming_soon.html',
       flashcards: 'https://notebook.google.com/notebook/efcfa089-f70a-432a-a090-a992ecdbfadf/artifact/e594ad86-904f-4c55-acf6-b6c022b922b6?utm_source=nlm_web_share&utm_medium=google_oo&utm_campaign=art_share_1&utm_content=&utm_smc=nlm_web_share_google_oo_art_share_1_',
      },
-
-
-
   },
 
 
